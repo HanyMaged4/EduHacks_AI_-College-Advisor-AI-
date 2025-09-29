413ace3ea7689e17c4ff77143cfa3fc14943de00
HanyMaged4 <hany.maged9102@gmail.com>
2025-09-29 10:36:14 +0300

    fix

--- Backend/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from "./app.service";
-
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
